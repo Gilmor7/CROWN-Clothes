b8dc43f9b9c56ef1187945ed5b932aaa0ce668b5
Gil Morhaim <gilov1997@gmail.com>
2019-08-29 13:53:16 +0300

    creadted project with create-react-app and added files for homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Directory from '../components/Directory'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
